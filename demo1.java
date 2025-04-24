2997fe2ee31e91e3afbad9659844a037b12e6d5e
omkar25204 <suhasjakati143@gmail.com>
2025-04-24 11:28:10 +0530

    save

---
+++ demo1.java
@@ -0,0 +1,12 @@
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+        System.out.println("Addition: " + num1 + " + " + num2 + " = " + sum);
+
+        int difference = num1 - num2;
+        System.out.println("Subtraction: " + num1 + " - " + num2 + " = " + difference);
+    }
+}
